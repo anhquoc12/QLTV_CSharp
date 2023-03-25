9c3843792e8ce763aaf172402cde1f8e91535668
cuongtran1102 <116043618+cuongtran1102@users.noreply.github.com>
2023-03-25 21:06:44 +0700

    update 25/3/2023

--- LibraryManagement/QLTV.DAO/SachDAO.cs
+++ LibraryManagement/QLTV.DAO/SachDAO.cs
@@ -25,14 +25,27 @@ namespace QLTV.DAO
                         s.TheLoai,
                         s.GiaNhap,
                         t.TenTacGia,
-                        s.SoLuong
-                    }).ToList().Select(s => new {
+                        s.SoLuong,
+                        s.MaNCC
+                    }).Join(db.NhaCungCaps, c => c.MaNCC, n => n.MaNCC, (s, n) => new
+                    {
+                        s.MaSach,
+                        s.TenSach,
+                        s.TheLoai,
+                        s.GiaNhap,
+                        s.TenTacGia,
+                        s.SoLuong,
+                        n.TenNCC
+                    }).
+                    ToList().Select(s => new
+                    {
                         s.MaSach,
                         s.TenSach,
                         s.TheLoai,
                         GiaNhap = s.GiaNhap.ToString("#,##0.00"),
                         s.TenTacGia,
-                        s.SoLuong
+                        s.SoLuong,
+                       s.TenNCC
                     });
                 return query.ToList();
             }
